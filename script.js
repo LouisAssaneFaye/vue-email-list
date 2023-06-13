81cc56e238552d49536f3112e93dd6d1c0564b8a
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-13 17:06:18 +0200

    Esercizio terminato: manca solo il bonus

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const {createApp} = vue;
+const {createApp} = Vue;
 createApp({
     data(){
         return{
+            mailList : [
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                ''
+            ],
+
+            mail: ''
 
         }
 
     },
     methods: {
-        
+        getNewRandomEmail(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response)=>{
+                const result = response.data;
+                console.log(result);
+                this.mail=result.response
+            })
+        }
+
+    },
+    created() {
+        this.getNewRandomEmail()
+
     }
 }).mount('#app')
